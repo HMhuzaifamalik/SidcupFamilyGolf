25b466455bdfe7d77c648d9021acf4bbabb01f85
Huzaifa_Malik <huzaifamalik21775@gmail.com>
2024-10-30 18:31:20 +0500

    nearly completed

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ document.addEventListener("mousemove",function(dets){
     blur.style.top =dets.y-200 +"px"
 
 })
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
+  });
+});
 
 gsap.to("#nav", {
     backgroundColor: "#000",
@@ -33,3 +46,67 @@ gsap.to("#nav", {
       scrub: 2,
     },
   });
+  gsap.from("#about-us img, #about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+      trigger:"#about-us",
+      scroller:"body",
+      // markers:true,
+      start:"top 70%",
+      end:"top 65%",
+      scrub:1
+    }
+  });
+  gsap.from(".card",{
+    scale:0.8,
+    // opacity:0,
+    duration:1,
+    stagger:0.1,
+    scrollTrigger:{
+      trigger:".card",
+      scroller:"body",
+      // markers:true,
+      start:"top 60%",
+      end:"top 58%",
+      scrub:1
+    }
+  });
+  gsap.from("#quote-left",{
+     y:-70,
+     x:-70,
+     scrollTrigger:{
+       trigger:"#quote-left",
+       scroller:"body",
+      //  markers:true,
+       start:"top 55%",
+       end:"top 45%",
+       scrub:4,
+     }
+  })
+  gsap.from("#quote-right",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+      trigger:"#quote-left",
+      scroller:"body",
+      // markers:true,
+      start:"top 55%",
+      end:"top 45%",
+      scrub:4,
+    }
+ })
+ gsap.from("#page4 h1",{
+  y:50,
+  scrollTrigger:{
+    trigger:"#page4 h1",
+    scroller:"body",
+    // markers:true,
+    start:"top 75%",
+    end:"top 75%",
+    scrub:3,
+  }
+ 
+ })
+  
